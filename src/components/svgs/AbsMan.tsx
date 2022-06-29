688328c234c30d571e34a1d3a7dd918744d327f4
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-06-29 16:58:23 -0700

    added abs,bicep, and quadricepts svg

---
+++ src/components/svgs/AbsMan.tsx
@@ -0,0 +1,239 @@
+export default function AbsMan({
+  width,
+  height,
+}: {
+  width?: string;
+  height?: string;
+}) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 707 489"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <ellipse
+        rx="83.5783"
+        ry="64.8264"
+        transform="matrix(0.854805 0.51895 -0.486791 0.873519 527 100)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        rx="83.5783"
+        ry="57.715"
+        transform="matrix(0.854805 0.51895 -0.486791 0.873519 530.462 93.7881)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        rx="87.8038"
+        ry="68.1039"
+        transform="matrix(0.533186 0.845998 -0.825577 0.564289 603.082 201.288)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        rx="87.8038"
+        ry="60.6329"
+        transform="matrix(0.533186 0.845998 -0.825577 0.564289 609.25 197.072)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        rx="84.1483"
+        ry="57.843"
+        transform="matrix(-0.856929 0.515434 0.490242 0.871587 182.293 110.788)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        rx="91.9427"
+        ry="71.5776"
+        transform="matrix(-0.536729 0.843755 0.828005 0.560721 108.996 218.288)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        rx="91.9427"
+        ry="63.7256"
+        transform="matrix(-0.536729 0.843755 0.828005 0.560721 102.495 213.885)"
+        fill="#F9D5B4"
+      />
+      <ellipse cx="358" cy="227" rx="154" ry="182" fill="#E9BE97" />
+      <ellipse cx="358" cy="216.5" rx="154" ry="171.5" fill="#F9D5B4" />
+      <ellipse cx="355" cy="62.5" rx="117" ry="42.5" fill="#E9BE97" />
+      <rect x="292" y="9" width="132" height="53" fill="#E9BE97" />
+      <rect x="218" y="252" width="288" height="237" rx="80" fill="#E9BE97" />
+      <rect x="219" y="248" width="286" height="231" rx="80" fill="#F9D5B4" />
+      <ellipse cx="268.5" cy="154.5" rx="89.5" ry="100.5" fill="#E9BE97" />
+      <ellipse cx="447.5" cy="145.5" rx="89.5" ry="100.5" fill="#E9BE97" />
+      <ellipse cx="268.5" cy="150.5" rx="89.5" ry="88.5" fill="#F9D5B4" />
+      <ellipse cx="447.5" cy="141.5" rx="89.5" ry="87.5" fill="#F9D5B4" />
+      <ellipse
+        cx="299.194"
+        cy="70.3162"
+        rx="62.6467"
+        ry="36.0223"
+        transform="rotate(-13.1351 299.194 70.3162)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="231.194"
+        cy="96.3162"
+        rx="62.6467"
+        ry="36.0223"
+        transform="rotate(-13.1351 231.194 96.3162)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="491.106"
+        cy="93.7274"
+        rx="62.6467"
+        ry="36.0223"
+        transform="rotate(33.6988 491.106 93.7274)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="416.494"
+        cy="73.0592"
+        rx="61.8226"
+        ry="36.0223"
+        transform="rotate(16.4554 416.494 73.0592)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="352.015"
+        cy="83.1643"
+        rx="47.5"
+        ry="36.0223"
+        transform="rotate(23.8734 352.015 83.1643)"
+        fill="#F9D5B4"
+      />
+      <rect x="304" y="9" width="109" height="53" fill="#F9D5B4" />
+      <ellipse
+        cx="322.5"
+        cy="265"
+        rx="35.5"
+        ry="19"
+        transform="rotate(-2.87431 322.5 265)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        cx="322.324"
+        cy="261.504"
+        rx="35.5"
+        ry="15.5"
+        transform="rotate(-2.87431 322.324 261.504)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="396.596"
+        cy="264.752"
+        rx="35.5"
+        ry="19"
+        transform="rotate(11.6027 396.596 264.752)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        cx="397.3"
+        cy="261.323"
+        rx="35.5"
+        ry="15.5"
+        transform="rotate(11.6027 397.3 261.323)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="322.408"
+        cy="300"
+        rx="35.5"
+        ry="19"
+        transform="rotate(-2.87431 322.408 300)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        cx="322.233"
+        cy="296.504"
+        rx="35.5"
+        ry="15.5"
+        transform="rotate(-2.87431 322.233 296.504)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="396.504"
+        cy="299.752"
+        rx="35.5"
+        ry="19"
+        transform="rotate(11.6027 396.504 299.752)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        cx="397.208"
+        cy="296.323"
+        rx="35.5"
+        ry="15.5"
+        transform="rotate(11.6027 397.208 296.323)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="322.408"
+        cy="340"
+        rx="35.5"
+        ry="19"
+        transform="rotate(11.5877 322.408 340)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        cx="323.111"
+        cy="336.571"
+        rx="35.5"
+        ry="15.5"
+        transform="rotate(11.5877 323.111 336.571)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="396.504"
+        cy="339.752"
+        rx="35.5"
+        ry="19"
+        transform="rotate(-5.01254 396.504 339.752)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        cx="396.198"
+        cy="336.265"
+        rx="35.5"
+        ry="15.5"
+        transform="rotate(-5.01254 396.198 336.265)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="322.408"
+        cy="376"
+        rx="35.5"
+        ry="19"
+        transform="rotate(10.8562 322.408 376)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        cx="323.067"
+        cy="372.563"
+        rx="35.5"
+        ry="15.5"
+        transform="rotate(10.8562 323.067 372.563)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        cx="396.504"
+        cy="375.752"
+        rx="35.5"
+        ry="19"
+        transform="rotate(-4.26841 396.504 375.752)"
+        fill="#E9BE97"
+      />
+      <ellipse
+        cx="396.244"
+        cy="372.261"
+        rx="35.5"
+        ry="15.5"
+        transform="rotate(-4.26841 396.244 372.261)"
+        fill="#F9D5B4"
+      />
+    </svg>
+  );
+}
---
+++ src/components/svgs/Quadriceps.tsx
@@ -0,0 +1,144 @@
+import React from "react";
+
+export default function Quadriceps({
+  width,
+  height,
+}: {
+  width?: string;
+  height?: string;
+}) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 568 717"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <ellipse cx="143.5" cy="584.5" rx="123.5" ry="132.5" fill="#E9BE97" />
+      <ellipse cx="143.5" cy="584.5" rx="112.5" ry="132.5" fill="#F9D5B4" />
+      <ellipse cx="149" cy="225.5" rx="129" ry="173.5" fill="#E9BE97" />
+      <ellipse cx="149" cy="229" rx="129" ry="170" fill="#F9D5B4" />
+      <ellipse cx="154" cy="488.5" rx="62" ry="62.5" fill="#E9BE97" />
+      <ellipse cx="154" cy="476" rx="53" ry="65" fill="#F9D5B4" />
+      <ellipse cx="205" cy="303" rx="79" ry="174" fill="#E9BE97" />
+      <ellipse cx="83.5" cy="303" rx="83.5" ry="174" fill="#E9BE97" />
+      <ellipse cx="205" cy="287.5" rx="79" ry="174.5" fill="#E9BE97" />
+      <ellipse cx="83.5" cy="287.5" rx="83.5" ry="174.5" fill="#E9BE97" />
+      <ellipse
+        cx="70.3117"
+        cy="160.897"
+        rx="32.5"
+        ry="59.6298"
+        transform="rotate(27.4243 70.3117 160.897)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        rx="32.4946"
+        ry="59.6298"
+        transform="matrix(-0.869179 0.494498 0.494498 0.869179 217.73 160.897)"
+        fill="#F9D5B4"
+      />
+      <ellipse cx="202.5" cy="294" rx="75.5" ry="174" fill="#F9D5B4" />
+      <ellipse cx="86" cy="294" rx="80" ry="174" fill="#F9D5B4" />
+      <ellipse cx="149.5" cy="225.5" rx="66.5" ry="173.5" fill="#E9BE97" />
+      <ellipse cx="149.5" cy="229" rx="57.5" ry="170" fill="#F9D5B4" />
+      <ellipse cx="149.5" cy="229" rx="66.5" ry="116" fill="#F9D5B4" />
+      <ellipse cx="143.5" cy="584.5" rx="123.5" ry="132.5" fill="#E9BE97" />
+      <ellipse cx="143.5" cy="584.5" rx="112.5" ry="132.5" fill="#F9D5B4" />
+      <ellipse cx="149" cy="225.5" rx="129" ry="173.5" fill="#E9BE97" />
+      <ellipse cx="149" cy="229" rx="129" ry="170" fill="#F9D5B4" />
+      <ellipse cx="154" cy="488.5" rx="62" ry="62.5" fill="#E9BE97" />
+      <ellipse cx="154" cy="476" rx="53" ry="65" fill="#F9D5B4" />
+      <ellipse cx="205" cy="303" rx="79" ry="174" fill="#E9BE97" />
+      <ellipse cx="83.5" cy="303" rx="83.5" ry="174" fill="#E9BE97" />
+      <ellipse cx="205" cy="287.5" rx="79" ry="174.5" fill="#E9BE97" />
+      <ellipse cx="83.5" cy="287.5" rx="83.5" ry="174.5" fill="#E9BE97" />
+      <ellipse
+        cx="70.3117"
+        cy="160.897"
+        rx="32.5"
+        ry="59.6298"
+        transform="rotate(27.4243 70.3117 160.897)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        rx="32.4946"
+        ry="59.6298"
+        transform="matrix(-0.869179 0.494498 0.494498 0.869179 217.73 160.897)"
+        fill="#F9D5B4"
+      />
+      <ellipse cx="202.5" cy="294" rx="75.5" ry="174" fill="#F9D5B4" />
+      <ellipse cx="86" cy="294" rx="80" ry="174" fill="#F9D5B4" />
+      <ellipse cx="149.5" cy="225.5" rx="66.5" ry="173.5" fill="#E9BE97" />
+      <ellipse cx="149.5" cy="229" rx="57.5" ry="170" fill="#F9D5B4" />
+      <ellipse cx="149.5" cy="229" rx="66.5" ry="116" fill="#F9D5B4" />
+      <ellipse cx="427.5" cy="584.5" rx="123.5" ry="132.5" fill="#E9BE97" />
+      <ellipse cx="427.5" cy="584.5" rx="112.5" ry="132.5" fill="#F9D5B4" />
+      <ellipse cx="433" cy="225.5" rx="129" ry="173.5" fill="#E9BE97" />
+      <ellipse cx="433" cy="229" rx="129" ry="170" fill="#F9D5B4" />
+      <ellipse cx="438" cy="488.5" rx="62" ry="62.5" fill="#E9BE97" />
+      <ellipse cx="438" cy="476" rx="53" ry="65" fill="#F9D5B4" />
+      <ellipse cx="489" cy="303" rx="79" ry="174" fill="#E9BE97" />
+      <ellipse cx="367.5" cy="303" rx="83.5" ry="174" fill="#E9BE97" />
+      <ellipse cx="489" cy="287.5" rx="79" ry="174.5" fill="#E9BE97" />
+      <ellipse cx="367.5" cy="287.5" rx="83.5" ry="174.5" fill="#E9BE97" />
+      <ellipse
+        cx="354.312"
+        cy="160.897"
+        rx="32.5"
+        ry="59.6298"
+        transform="rotate(27.4243 354.312 160.897)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        rx="32.4946"
+        ry="59.6298"
+        transform="matrix(-0.869179 0.494498 0.494498 0.869179 501.73 160.897)"
+        fill="#F9D5B4"
+      />
+      <ellipse cx="486.5" cy="294" rx="75.5" ry="174" fill="#F9D5B4" />
+      <ellipse cx="370" cy="294" rx="80" ry="174" fill="#F9D5B4" />
+      <ellipse cx="433.5" cy="225.5" rx="66.5" ry="173.5" fill="#E9BE97" />
+      <ellipse cx="433.5" cy="229" rx="57.5" ry="170" fill="#F9D5B4" />
+      <ellipse cx="433.5" cy="229" rx="66.5" ry="116" fill="#F9D5B4" />
+      <ellipse cx="427.5" cy="584.5" rx="123.5" ry="132.5" fill="#E9BE97" />
+      <ellipse cx="427.5" cy="584.5" rx="112.5" ry="132.5" fill="#F9D5B4" />
+      <ellipse cx="433" cy="225.5" rx="129" ry="173.5" fill="#E9BE97" />
+      <ellipse cx="433" cy="229" rx="129" ry="170" fill="#F9D5B4" />
+      <ellipse cx="438" cy="488.5" rx="62" ry="62.5" fill="#E9BE97" />
+      <ellipse cx="438" cy="476" rx="53" ry="65" fill="#F9D5B4" />
+      <ellipse cx="489" cy="303" rx="79" ry="174" fill="#E9BE97" />
+      <ellipse cx="367.5" cy="303" rx="83.5" ry="174" fill="#E9BE97" />
+      <ellipse cx="489" cy="287.5" rx="79" ry="174.5" fill="#E9BE97" />
+      <ellipse cx="367.5" cy="287.5" rx="83.5" ry="174.5" fill="#E9BE97" />
+      <ellipse
+        cx="354.312"
+        cy="160.897"
+        rx="32.5"
+        ry="59.6298"
+        transform="rotate(27.4243 354.312 160.897)"
+        fill="#F9D5B4"
+      />
+      <ellipse
+        rx="32.4946"
+        ry="59.6298"
+        transform="matrix(-0.869179 0.494498 0.494498 0.869179 501.73 160.897)"
+        fill="#F9D5B4"
+      />
+      <ellipse cx="486.5" cy="294" rx="75.5" ry="174" fill="#F9D5B4" />
+      <ellipse cx="370" cy="294" rx="80" ry="174" fill="#F9D5B4" />
+      <ellipse cx="433.5" cy="225.5" rx="66.5" ry="173.5" fill="#E9BE97" />
+      <ellipse cx="433.5" cy="229" rx="57.5" ry="170" fill="#F9D5B4" />
+      <ellipse cx="433.5" cy="229" rx="66.5" ry="116" fill="#F9D5B4" />
+      <path
+        d="M311.685 179.612C305.405 191.182 272.875 184.432 257.395 179.612C211.826 140.048 117.681 60.921 105.645 60.921C93.61 60.921 100.631 23.6847 105.645 5.06659C221.42 1.24322 455.193 -4.10949 464.088 5.06659C472.984 14.2427 467.795 46.1263 464.088 60.921C415.903 95.6638 317.964 168.042 311.685 179.612Z"
+        fill="#4BD0B8"
+      />
+      <path
+        d="M311.685 169.955C305.405 180.903 272.875 174.517 257.395 169.955C211.826 132.519 117.681 57.6457 105.645 57.6457C93.61 57.6457 100.631 22.4114 105.645 4.79419C221.42 1.17638 455.193 -3.88855 464.088 4.79419C472.984 13.4769 467.795 43.6463 464.088 57.6457C415.903 90.5206 317.964 159.007 311.685 169.955Z"
+        fill="#66E4CE"
+      />
+    </svg>
+  );
+}
