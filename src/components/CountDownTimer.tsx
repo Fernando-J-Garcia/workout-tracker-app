0c0fed513d8d94f3b7cb32da8a1ece036e3be176
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-06-29 10:15:52 -0700

    added countdown timer fuctionality

--- src/components/CountDownTimer.tsx
+++ src/components/CountDownTimer.tsx
@@ -1,15 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
+import { Exercise } from "../Types/exercise";
 
-export default function CountDownTimer() {
-  const [isTimerPaused, setIsTimerPaused] = useState(true);
+interface CountDownTimerInterface {
+  currentExercise: Exercise;
+}
+interface Time {
+  seconds: string;
+  minutes: string;
+  hours: string;
+}
+function FormatTime(timeInSeconds: string): Time {
+  let length = parseInt(timeInSeconds);
+  let hours = 0;
+  let minutes = 0;
+  let seconds = length;
+
+  hours = Math.floor(length / 3600);
+  length -= 3600 * hours;
+  minutes = Math.floor(length / 60);
+  length -= 60 * minutes;
+  seconds = length;
+
+  let hoursString = "0";
+  let minutesString = "0";
+  let secondsString = "0";
+
+  if (hours > 9) hoursString = hours.toString();
+  else hoursString += hours;
+  if (minutes > 9) minutesString = minutes.toString();
+  else minutesString += minutes;
+  if (seconds > 9) secondsString = seconds.toString();
+  else secondsString += seconds;
+  return {
+    seconds: secondsString,
+    minutes: minutesString,
+    hours: hoursString,
+  };
+}
+
+export default function CountDownTimer({
+  currentExercise,
+}: CountDownTimerInterface) {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [timeInSeconds, setTimeInSeconds] = useState<number>(
+    parseInt(currentExercise.repLengthInSeconds)
+  );
+  const [time, setTime] = useState<Time>(
+    FormatTime(currentExercise.repLengthInSeconds)
+  );
+  const [timer, setTimer] = useState<NodeJS.Timer>();
+
+  useEffect(() => {
+    if (isPlaying) {
+      setTimer(setInterval(countDown, 1000));
+    } else {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [isPlaying]);
+
+  function countDown() {
+    setTimeInSeconds((prev) => {
+      const newTimeInSeconds = prev - 1;
+      setTime(FormatTime(newTimeInSeconds.toString()));
+      return newTimeInSeconds;
+    });
+  }
   function startTimer() {
-    setIsTimerPaused(false);
+    setIsPlaying(true);
   }
   function stopTimer() {
-    setIsTimerPaused(true);
+    setIsPlaying(false);
   }
-  if (isTimerPaused)
+  if (!isPlaying)
     return (
       <Button
         className="rounded-circle border border-2 border-primary"
@@ -28,7 +92,7 @@ export default function CountDownTimer() {
         style={{ width: "200px", height: "200px" }}
         onClick={stopTimer}
       >
-        <h1>00:00</h1>
+        <h1>{`${time.minutes}:${time.seconds}`}</h1>
       </Button>
     );
 }
