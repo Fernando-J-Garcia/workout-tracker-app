6e6f1a25359ea9be9439d7c050208a269b3538c8
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-07-02 10:44:43 -0700

    added wip ui to profile page

--- src/pages/Progress.tsx
+++ src/pages/Progress.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import ConstructionMan from "../components/svgs/ConstructionMan";
 
 export default function Progress() {
   return (
     <div>
-      <h1>Progress</h1>
+      <h1>Work In Progress...</h1>
+      <Container>
+        <ConstructionMan />
+      </Container>
     </div>
   );
 }
